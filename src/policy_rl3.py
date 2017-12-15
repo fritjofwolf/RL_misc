c2a000f34aa3affc5941bad8d69a7438fa4c21eb
Fritjof Wolf <fritjofwolf@gmail.com>
2017-12-16 00:40:01 +0100

    backup

--- src/policy_rl3.py
+++ src/policy_rl3.py
@@ -12,28 +12,28 @@ def sigmoid(x):
 def evaluateWeights(weights):
     global env
     sum = 0
-    iterations = 100
+    iterations = 10
     for _ in range(iterations):
         state = env.reset()
         done = False
         while not done:
-            print('sigmoid is', sigmoid(np.inner(weights, state)))
+            #print('sigmoid is', sigmoid(np.inner(weights, state)))
             if sigmoid(np.inner(weights, state)) > np.random.rand():
                 state, reward, done, info = env.step(1)
             else:
                 state, reward, done, info = env.step(0)
             sum += 1
-    return sum // iterations
+    return sum / iterations
 
 def compute_gradient(weights):
     eps = 1
     gradient = np.zeros(4)
     old_value = evaluateWeights(weights)
-    for k in range(4):
+    for k in range(0,4):
         tmp_weights = weights.copy()
         tmp_weights[k] += eps
         new_value = evaluateWeights(tmp_weights)
-        gradient[k] = (old_value - new_value) / eps
+        gradient[k] = (new_value - old_value) / eps
     return gradient
 
 best_weights = np.zeros(4)
@@ -42,7 +42,7 @@ weights = np.zeros(4)
 for i in range(10000):
     gradient = compute_gradient(weights)
     print(weights, gradient)
-    weights += 1*gradient
+    weights += 0.5*gradient
     score = evaluateWeights(weights)
     if score > best_score:
         best_score = score
