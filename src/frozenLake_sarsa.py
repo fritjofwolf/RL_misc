b8429b7097deafcd1b3068557f4086d6001d3a5b
Fritjof Wolf <fritjofwolf@gmail.com>
2018-03-13 08:36:47 +0100

    first version of sarsa

--- src/frozenLake_sarsa.py
+++ src/frozenLake_sarsa.py
@@ -4,36 +4,47 @@ import numpy as np
 env = gym.make('FrozenLake-v0')
 cnt_visited = np.zeros((16,4))
 
-def play_game():
+def play_game(q_function, cnt_visited):
     global env
-    global cnt_visited
-    global q_function
-    for _ in range(10000):
+    sum_return = 0
+    for _ in range(100000):
         state = env.reset()
-        action = compute_new_action(state, np.random.randint(4))
+        action = compute_new_action_eps_greedy(state, np.random.randint(4))
         done = False
         while not done:
             new_state, reward, done, info = env.step(action)
-            new_action = compute_new_action(new_state, action)
+            #if done:
+                #print(state, new_state, reward)
+            new_action = compute_new_action_eps_greedy(new_state, action)
             ret = (reward+0.9*q_function[new_state,new_action] - q_function[state, action])
+            #if ret > 0:
+                #print(ret, state, new_state)
             q_function[state, action] += 0.1* ret
             cnt_visited[state, action] += 1
             state = new_state
             action = new_action
+            sum_return += reward
+    print('Average reward per episode is:',sum_return/100000)
+    return q_function, cnt_visited
 
-def compute_new_action(current_state, current_action):
+def compute_new_action_eps_greedy(current_state, current_action):
     global cnt_visited
     global q_function
     action = 0
     epsilon = 0.1
-    if np.random.rand() < epsilon / cnt_visited[current_state, current_action]:
+#    if np.random.rand() < epsilon / (cnt_visited[current_state, current_action]+1):
+    if np.random.rand() < epsilon:
         action = np.random.randint(0,4)
     else:
         action = np.argmax(q_function[current_state,:])
     return action
 
+def compute_new_action_random(current_state, current_action):
+    return np.random.randint(0,4)
+
 q_function = np.zeros((16,4))
-play_game()
+cnt_visited = np.zeros((16,4))
+play_game(q_function, cnt_visited)
 print('Q-Function ist:')
 print(q_function)
 print('Cnt of States visited is:')
