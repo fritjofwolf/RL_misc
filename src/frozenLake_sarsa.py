715263f3f4b25e35416ea950c0e2675ab9630448
Fritjof Wolf <fritjofwolf@gmail.com>
2018-03-19 23:05:56 +0100

    small changes

--- src/frozenLake_sarsa.py
+++ src/frozenLake_sarsa.py
@@ -6,5 +6,5 @@ import rl_methods as rlm
 if __name__ == '__main__':
     env = gym.make('FrozenLake-v0')
     q_function = np.zeros((16,4))
-    q_function = rlm.train_qfunction_with_td0(q_function, env, 10000, 0.1, 0.9)
-    rlm.evaluate_qfunction(q_function, env)
+    q_function = rlm.train_qfunction_with_mc(q_function, env, 100000, 0.1, 0.9)
+    rlm.evaluate_qfunction(q_function, env, 10000)
