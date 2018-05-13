159c1fdea1d2b367141bd2a85c6d3d9a4cd86ca6
Fritjof Wolf <fritjofwolf@gmail.com>
2018-05-13 22:31:08 +0200

    added replay memory and negative reward for episode end

---
+++ src/approximation_methods_nn.py
@@ -0,0 +1,114 @@
+import numpy as np
+import progressbar
+from collections import deque
+import random
+
+state_space = 5
+action_space = 2
+
+def train_linear_qfunction_with_td0(mlp, env, iter, alpha, gamma):
+    bar = progressbar.ProgressBar()
+    mlp = do_initial_fit(mlp, env)
+    replay_mem = deque(maxlen = 10000)
+    for i in bar(range(iter)):
+        #if i % 100 == 0 and i != 0 or i == 1:
+            #print('Die Gewichte und Biase nach ' + str(i)+ ' iterationen sind:', mlp.coefs_, mlp.intercepts_)
+        state = env.reset()
+        action = compute_new_action_eps_greedy(state, mlp)
+        done = False
+        old_qvalue = compute_qfunction(mlp, state)
+        cnt = 0
+        while not done:
+            # env.render()
+            new_state, reward, done, info = env.step(action)
+            if done:
+                reward = -10
+            new_action = compute_new_action_eps_greedy(new_state, mlp)
+            #print(new_action)
+            new_qvalue = compute_qfunction(mlp, new_state)
+            #print(compute_qfunction(mlp, new_state))
+            target = compute_target(old_qvalue, new_qvalue, gamma, reward, action, new_action)
+            replay_mem.append((state, target))
+            x, y = get_training_set(replay_mem)
+            mlp.partial_fit(x, y)
+            state, action = new_state, new_action
+            old_qvalue = new_qvalue
+            cnt += 1
+        print(cnt)
+    return mlp
+
+def get_training_set(replay_mem):
+    x = []
+    y = []
+    for i in range(100):
+        (a,b) = replay_mem[random.randrange(len(replay_mem))]
+        x.append(a)
+        y.append(b)
+    return x, y
+        
+
+def compute_target(old_qvalue, new_qvalue, gamma, reward, action, new_action):
+    ret = (reward+gamma*new_qvalue[new_action] - old_qvalue[action])
+    target = old_qvalue.copy()
+    target[action] = ret
+    return target
+
+def do_initial_fit(mlp, env):
+    state = env.reset()
+    mlp.partial_fit([state], [[0,0]])
+    return mlp
+
+def evaluate_qfunction(mlp, env, iter):
+    sum_return = 0
+    for _ in range(iter):
+        episode_reward = 0
+        state = env.reset()
+        action = compute_new_action_greedy(state, mlp)
+        done = False
+        while not done:
+            #env.render()
+            state, reward, done, info = env.step(action)
+            action = compute_new_action_greedy(state, mlp)
+            sum_return += reward
+            episode_reward += reward
+        #print(episode_reward)
+    print('Average reward per episode is:', sum_return/iter)
+
+
+def compute_new_action_greedy(state, mlp):
+    action_values = mlp.predict([state])
+    action = np.argmax(action_values)
+    return action
+
+
+def compute_new_action_eps_greedy(state, mlp):
+    if np.random.rand() < 0.3:
+        action = np.random.randint(action_space)
+    else:
+        action_values = mlp.predict([state])
+        action = np.argmax(action_values)
+    return action
+
+
+def compute_qfunction(mlp, state):
+    return mlp.predict([state])[0]
+
+def create_feature_vector_cartpole(state, action):
+    feature_vector = np.zeros(action_space*state_space)
+    state = np.append(state, 1)
+    if action == 0:
+        feature_vector[:state_space] = state
+    elif action == 1:
+        feature_vector[state_space:] = state
+    return feature_vector
+
+def create_feature_vector_mountaincar(state, action):
+    feature_vector = np.zeros(action_space*state_space)
+    state = np.append(state, 1)
+    if action == 0:
+        feature_vector[:state_space] = state
+    elif action == 1:
+        feature_vector[state_space:2*state_space] = state
+    elif action == 2:
+        feature_vector[2*state_space:]
+    return feature_vector
